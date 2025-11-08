08b9586950e411de9914f5f3c96d1f882187a47c
ygweygyigyigyigerig <192640157+ygweygyigyigyigerig@users.noreply.github.com>
2025-11-08 15:43:35 +0100

    Added structure

---
+++ learningrust/src/main.rs
@@ -0,0 +1,81 @@
+use std::fs;
+use std::fs::File;
+use std::io;
+use std::io::Write;
+use std::io::prelude::*;
+use std::path::Path;
+use std::path::PathBuf;
+
+fn set_path_and_check_if_exists() -> io::Result<PathBuf> {
+    let dir = PathBuf::from("/home/simon/secret-passwords");
+    fs::create_dir_all(&dir)?;
+    Ok(dir)
+}
+
+fn read_input_int() -> u32 {
+    let mut input = String::new();
+    let stdin = io::stdin();
+    stdin.read_line(&mut input);
+
+    input
+        .trim()
+        .parse::<u32>()
+        .expect("Please enter a valid number")
+}
+
+fn read_input_str() -> String {
+    let mut input = String::new();
+    let stdin = io::stdin();
+    stdin.read_line(&mut input);
+    input.trim().to_string()
+}
+
+fn create_passwd() -> std::io::Result<()> {
+    println!("Chose name for your password: ");
+    let name_of_passwd: String = read_input_str();
+    let mut file = File::create(&name_of_passwd)?;
+
+    let dir = set_path_and_check_if_exists()?;
+    let path = dir.join(name_of_passwd);
+
+    println!("Your password: ");
+    let passwd = read_input_str();
+    fs::write(&path, passwd)?;
+    Ok(())
+}
+
+fn remove_psswd() -> std::io::Result<()> {
+    // Intialize dir, to later join name_of_passwd_to_del later on.
+    let dir = set_path_and_check_if_exists()?;
+
+    println!("Enter name of password you'd like to delete: ");
+    let name_of_passwd_to_del: String = read_input_str();
+    let passwd = dir.join(&name_of_passwd_to_del);
+    fs::remove_file(passwd)?;
+    Ok(())
+}
+
+fn list_passwds() {}
+
+fn main() -> std::io::Result<()> {
+    println!("***************************");
+    println!("Welcome in password manager");
+    println!("***************************");
+
+    println!("1. Create password");
+    println!("2. Remove password");
+    println!("3. List all passwords");
+    println!("4. Exit\n");
+    println!("Chose your action: \n");
+
+    let action: u32 = read_input_int();
+
+    match action {
+        1 => create_passwd()?,
+        2 => remove_psswd()?,
+        3 => println!("E"),
+        4 => return Ok(()),
+        _ => println!("Chose from 1-4"),
+    }
+    Ok(())
+}
