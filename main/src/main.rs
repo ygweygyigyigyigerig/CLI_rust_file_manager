30c6b80fa8b70c08f5878317f4ed0065dc99316c
ygweygyigyigyigerig <192640157+ygweygyigyigyigerig@users.noreply.github.com>
2025-11-18 20:53:18 +0100

    Base for new autoload handling

--- main/src/main.rs
+++ main/src/main.rs
@@ -3,19 +3,14 @@ use std::io;
 use std::path::Path;
 use std::path::PathBuf;
 
-fn check_for_dir_save(dir: &mut str, is_dir_set_true: &mut bool) {
+fn check_for_dir_save(dir: &mut PathBuf, is_dir_set_true: &mut bool) {
     match fs::exists("saved_dir") {
         Ok(true) => {
             println!("Saved directory dected, would you like to load it?");
             let user_choice = read_input_str().to_lowercase();
 
             if user_choice == "yes" || user_choice == "y" {
-                let exe_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
-                    .join("main")
-                    .with_file_name("saved_dir");
-                println!("{}", exe_dir.display());
-                let content = exe_dir.display().to_string();
-                std::fs::write(&dir, &content).expect("Crashy");
+                *dir = PathBuf::from("saved_dir");
                 *is_dir_set_true = true;
             }
         }
@@ -24,33 +19,28 @@ fn check_for_dir_save(dir: &mut str, is_dir_set_true: &mut bool) {
     }
 }
 
-fn save_dir(dir: &str) {
-    let mut exe_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    exe_dir.push("main");
-    exe_dir.set_file_name("saved_dir");
-    std::fs::write(&exe_dir, &dir).expect("boo you crashed or sum");
-    println!("{}", exe_dir.display());
+fn save_dir(dir: &mut PathBuf) {
+    *dir = PathBuf::from("saved_dir");
 }
 
-fn set_dir() -> String {
+fn set_dir() -> PathBuf {
     println!("Chose directory, where passwords will be saved ");
 
     loop {
-        let dir = read_input_str();
-
+        let mut dir: PathBuf = read_input_str().into();
         if Path::new(&dir).exists() {
-            println!("Path '{}' exists! Path updated sucesfully.", dir);
+            println!("Path '{}' exists! Path updated sucesfully.", dir.display());
             println!(
                 "Would you like to save directory, so it will be loaded automatically next time? (Yes/No)"
             );
             let user_choice = read_input_str().to_lowercase();
 
             if user_choice == "yes" || user_choice == "y" {
-                save_dir(&dir);
+                save_dir(&mut dir);
             }
             return dir;
         } else {
-            println!("Path '{}' does not exist! Retry\n", dir);
+            println!("Path '{}' does not exist! Retry\n", dir.display());
             continue;
         }
     }
@@ -87,11 +77,11 @@ fn read_input_str() -> String {
     }
 }
 
-fn create_passwd(dir: &str) -> std::io::Result<()> {
+fn create_passwd(dir: &PathBuf) -> std::io::Result<()> {
     println!("Chose name for your password: ");
     let name_of_passwd: String = read_input_str();
 
-    let path: PathBuf = PathBuf::from(dir).join(&name_of_passwd);
+    let path = PathBuf::from(dir).join(&name_of_passwd);
 
     println!("Your password: ");
     let passwd = read_input_str();
@@ -100,7 +90,7 @@ fn create_passwd(dir: &str) -> std::io::Result<()> {
     Ok(())
 }
 
-fn remove_psswd(dir: &str) -> std::io::Result<()> {
+fn remove_psswd(dir: &PathBuf) -> std::io::Result<()> {
     // Intialize dir, to later join name_of_passwd_to_del later on.
     println!("Enter name of password you'd like to delete: ");
     // get input
@@ -112,7 +102,7 @@ fn remove_psswd(dir: &str) -> std::io::Result<()> {
     Ok(())
 }
 
-fn list_passwds(dir: &str) -> std::io::Result<()> {
+fn list_passwds(dir: &PathBuf) -> std::io::Result<()> {
     for entry in fs::read_dir(&dir)? {
         let entry = entry?;
         let path = entry.path();
@@ -122,7 +112,7 @@ fn list_passwds(dir: &str) -> std::io::Result<()> {
 }
 fn main() -> std::io::Result<()> {
     let mut is_dir_set = false;
-    let mut dir = String::new();
+    let mut dir = PathBuf::new();
     check_for_dir_save(&mut dir, &mut is_dir_set);
 
     println!("***************************");
