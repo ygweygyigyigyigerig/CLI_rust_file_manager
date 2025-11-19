a3d9a4da8f4da2368686f987ff938b0647ba3f02
ygweygyigyigyigerig <192640157+ygweygyigyigyigerig@users.noreply.github.com>
2025-11-19 21:47:17 +0100

    Starting to experemiment with modules

---
+++ main/src/input.rs
@@ -0,0 +1,33 @@
+pub mod read_input {
+    use std::io;
+    pub fn read_int() -> io::Result<u32> {
+        let mut input = String::new();
+        let stdin = io::stdin();
+
+        loop {
+            input.clear();
+            stdin.read_line(&mut input)?;
+
+            if let Ok(num) = input.trim().parse::<u32>() {
+                break Ok(num);
+            }
+
+            println!("Invalid input! Try again.");
+        }
+    }
+
+    pub fn read_str() -> String {
+        let mut input = String::new();
+        let stdin = io::stdin();
+
+        loop {
+            match stdin.read_line(&mut input) {
+                Ok(_) => return input.trim().to_string(),
+                Err(e) => {
+                    println!("Error reading input: {}. Try again.", e);
+                    continue;
+                }
+            }
+        }
+    }
+}
